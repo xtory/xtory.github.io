06d13a08c94a974bf9d041e28086cde82246d1ee
Jerry <nightrk@hotmail.com>
2017-05-11 16:25:00 +0800

    add checking to polyfills to see if the scripts are running in web browsers or others (such as: Node.js)

--- xtory/dist/xtory.core.js
+++ xtory/dist/xtory.core.js
@@ -2,6 +2,43 @@ this.xtory = this.xtory || {};
 (function (exports) {
 'use strict';
 
+// Note:
+// Scripts could be run in web browsers or others (such as: Node.js). If they're
+// not running in web browsers (and hence 'window' doesn't exist), using 'window'
+// causes a reference error.
+
+var isRunningInWebBrowser = (
+    (typeof(window) !== 'undefined') ?
+    true :
+    false
+);
+
+if (isRunningInWebBrowser === true) {
+    //
+    // Note:
+    // Provides requestAnimationFrame in a cross browser way.
+    // @author paulirish / http://paulirish.com/
+
+    if (window.requestAnimationFrame === undefined) {
+        //
+        window.requestAnimationFrame = (function() {
+            //
+            return (
+                window.webkitRequestAnimationFrame ||
+                window.mozRequestAnimationFrame ||
+                window.oRequestAnimationFrame ||
+                window.msRequestAnimationFrame ||
+                function (
+                    callback, // function frameRequestCallback
+                    element   // DOMElement element
+                ){
+                    window.setTimeout(callback, 1000/60);
+                }
+            );
+        })();
+    }
+}
+
 //
 // Constructor.
 //
@@ -7668,9 +7705,6 @@ TransformedPositionTextureCoordinates.FRAGMENT_SHADER_SOURCE = (
 );
 
 // Polyfills.
-//import './polyfills.js';
-
-// Base.
 
 exports.ArrayHelper = ArrayHelper;
 exports.ComparisonResults = ComparisonResults;
--- xtory/src/core/polyfills.js
+++ xtory/src/core/polyfills.js
@@ -1,23 +1,36 @@
 // Note:
-// Provides requestAnimationFrame in a cross browser way.
-// @author paulirish / http://paulirish.com/
+// Scripts could be run in web browsers or others (such as: Node.js). If they're
+// not running in web browsers (and hence 'window' doesn't exist), using 'window'
+// causes a reference error.
 
-if (window !== undefined &&
-    window.requestAnimationFrame === undefined) {
+var isRunningInWebBrowser = (
+    (typeof(window) !== 'undefined') ?
+    true :
+    false
+);
+
+if (isRunningInWebBrowser === true) {
     //
-    window.requestAnimationFrame = (function() {
+    // Note:
+    // Provides requestAnimationFrame in a cross browser way.
+    // @author paulirish / http://paulirish.com/
+
+    if (window.requestAnimationFrame === undefined) {
         //
-        return (
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame ||
-            window.oRequestAnimationFrame ||
-            window.msRequestAnimationFrame ||
-            function (
-                callback, // function frameRequestCallback
-                element   // DOMElement element
-            ){
-                window.setTimeout(callback, 1000/60);
-            }
-        );
-    })();
+        window.requestAnimationFrame = (function() {
+            //
+            return (
+                window.webkitRequestAnimationFrame ||
+                window.mozRequestAnimationFrame ||
+                window.oRequestAnimationFrame ||
+                window.msRequestAnimationFrame ||
+                function (
+                    callback, // function frameRequestCallback
+                    element   // DOMElement element
+                ){
+                    window.setTimeout(callback, 1000/60);
+                }
+            );
+        })();
+    }
 }
